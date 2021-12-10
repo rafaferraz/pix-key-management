45c812baf7788e53008d72c546d4bb7d8f28b3fd
gustavosimoes <gustavoteixeira@gec.inatel.br>
2021-12-10 18:46:41 -0300

    fix

--- test/application_test.dart
+++ test/application_test.dart
@@ -1,6 +1,3 @@
-import 'dart:developer';
-
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:pix_key/controller/pix_key_controller.dart';
